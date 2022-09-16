207b5726551f86010401b6250536fad30480fce6
gn-show-show-way <104058374+gn-show-show-way@users.noreply.github.com>
2022-09-16 17:08:59 +0800

    update

--- ngpos/src/main/java/edu/scau/ngpos/controller/ProductController.java
+++ ngpos/src/main/java/edu/scau/ngpos/controller/ProductController.java
@@ -2,6 +2,7 @@ package edu.scau.ngpos.controller;
 
 import edu.scau.ngpos.domain.Category;
 import edu.scau.ngpos.domain.ProductDescription;
+import edu.scau.ngpos.service.IProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,25 +10,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @RestController
-@RequestMapping("productDescription")
+@RequestMapping("product")
 public class ProductController {
     @Autowired
-    private ProductDescription productDescription;
-    @Autowired
-    private Category category;
+    private IProductService productService;
 
-    @GetMapping("/{productId}")
-    public ProductDescription getById(@PathVariable("productId") Long productId){
-        productDescription.setProductId(productId);
-        productDescription.setProductName("文具");
-        productDescription.setProductSn("None");
-        productDescription.setPrice(BigDecimal.valueOf(0));
-        productDescription.setCategory(category);
-        category.setCategoryId(productId);
-        category.setCategoryName("文具");
+    @GetMapping("/{productSn}")
+    public ProductDescription getBySn(@PathVariable("productSn") String productSn){
+        return productService.getProductByProductSn(productSn);
+    }
 
-        return productDescription;
+    @GetMapping("/listAll")
+    public List<ProductDescription> listAllProduct(){
+        return productService.listAllProduct();
     }
 }
