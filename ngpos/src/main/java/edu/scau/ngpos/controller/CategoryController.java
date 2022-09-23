0771c97ca0139651fa88a9f3e9831a2fd816c0ff
gn-show-show-way <104058374+gn-show-show-way@users.noreply.github.com>
2022-09-23 19:57:16 +0800

    update

--- ngpos/src/main/java/edu/scau/ngpos/controller/CategoryController.java
+++ ngpos/src/main/java/edu/scau/ngpos/controller/CategoryController.java
@@ -2,6 +2,8 @@ package edu.scau.ngpos.controller;
 
 import edu.scau.ngpos.core.domain.AjaxResult;
 import edu.scau.ngpos.service.ICategoryService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,11 +16,13 @@ public class CategoryController {
     @Autowired
     private ICategoryService categoryService;
     @GetMapping("/{categoryId}")
+    @ApiOperation("根据id查询category")
     public AjaxResult getById(@PathVariable("categoryId") Long categoryId) {
         return AjaxResult.success(categoryService.getCategoryById(categoryId));
     }
 
     @GetMapping("/listAll")
+    @ApiOperation("查询所有category")
     public AjaxResult listAllCategory(){
         return AjaxResult.success(categoryService.listAllCategory());
     }
--- ngpos/src/main/java/edu/scau/ngpos/controller/ProductController.java
+++ ngpos/src/main/java/edu/scau/ngpos/controller/ProductController.java
@@ -3,6 +3,7 @@ package edu.scau.ngpos.controller;
 import edu.scau.ngpos.core.domain.AjaxResult;
 import edu.scau.ngpos.domain.ProductDescription;
 import edu.scau.ngpos.service.IProductService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,11 +19,13 @@ public class ProductController {
     private IProductService productService;
 
     @GetMapping("/{productSn}")
+    @ApiOperation("根据Sn查询product")
     public AjaxResult getBySn(@PathVariable("productSn") String productSn){
         return AjaxResult.success(productService.getProductByProductSn(productSn));
     }
 
     @GetMapping("/listAll")
+    @ApiOperation("查询所有product")
     public AjaxResult listAllProduct(){
         return AjaxResult.success(productService.listAllProduct());
     }
